d33b49de4f4fe44ddae7faec5c98f94507e7bc3f
nickpezzotti1 <nicholas.p.pezzotti@gmail.com>
2019-10-27 20:02:36 +0000

    value iteartion with manhattan distance

--- mdpAgents.py
+++ mdpAgents.py
@@ -36,28 +36,36 @@ import game
 import util
 
 import sys
-GHOST_REWARD = -200
+GHOST_REWARD = -100
 FOOD_REWARD = 10
 CAPSULE_REWARD = 10
-EMPTY_CELL_REWARD = -1
+EMPTY_CELL_REWARD = -0.5
 WALL_REWARD = 0
-NO_MOVE_REWARD = -1
 
 
-class MapRepresentation():
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    return type('Enum', (), enums)
+
+MazeEntity = enum("GHOST", "FOOD", "CAPSULE", "EMPTY_CELL", "WALL", "PACMAN")
+
+
+class Maze:
 
     def __init__(self, state):
-        self.update_map(state)
+        self.map = None
+        self.utilities = None
 
-    def update_map(self, state):
+        self.update(state)
+
+    def update(self, state):
         ''' Update the rewards on the map . Called after every move '''
-        self.map = self._get_empty_map(state)
-
-        self.pac_man_pos =  api.whereAmI(state)
-        self._update_rewards(state)
+        self.map = self._initialize(state)
+        self._fill(state)
+        self.utilities = None
         self._value_iteration(state)
 
-    def _get_empty_map(self, state):
+    def _initialize(self, state):
         ''' Generate an empty 2D-matrix representing the array '''
 
         corners = api.corners(state)
@@ -68,103 +76,104 @@ class MapRepresentation():
             max_row = max(coordinate[0], max_row)
             max_col = max(coordinate[1], max_col)
 
-        return [[EMPTY_CELL_REWARD for col in range(max_row + 1)] for col in range(max_col + 1)]
+        return [[MazeEntity.EMPTY_CELL for col in range(max_row + 1)] for col in range(max_col + 1)]
 
-    def _update_rewards(self, state):
-        ''' Update the board with current rewards '''
+    def _fill(self, state):
+        ''' Place the maze entities in the maze '''
 
+        # Place walls
         for wall in api.walls(state):
-            self.map[wall[1]][wall[0]] = WALL_REWARD
+            self.map[wall[1]][wall[0]] = MazeEntity.WALL
 
+        # Place foods
         for food in api.food(state):
-            self.map[food[1]][food[0]] = FOOD_REWARD
+            self.map[food[1]][food[0]] = MazeEntity.FOOD
 
+        # Place capsules
         for capsule in api.capsules(state):
-            self.map[capsule[1]][capsule[0]] = CAPSULE_REWARD
+            self.map[capsule[1]][capsule[0]] = MazeEntity.CAPSULE
 
+        # Place ghosts
         for ghost in api.ghosts(state):
-            self.map[int(ghost[1])][int(ghost[0])] = GHOST_REWARD
+            self.map[int(ghost[1])][int(ghost[0])] = MazeEntity.GHOST
 
+        # Place pacman player
         myPos = api.whereAmI(state)
-        self.map[myPos[1]][myPos[0]] = NO_MOVE_REWARD
+        self.map[myPos[1]][myPos[0]] = MazeEntity.PACMAN
 
-    def get_reward(self, row, col):
-        return self.map[row][col]
+    def get_reward(self, row, col, state):
+        if (self.map[row][col] == MazeEntity.WALL):
+            return "na"
+        elif (self.map[row][col] == MazeEntity.FOOD):
+            return FOOD_REWARD + self._manhattan_distance_to_closest_ghost(state, row, col)
+        elif (self.map[row][col] == MazeEntity.CAPSULE):
+            return CAPSULE_REWARD + self._manhattan_distance_to_closest_ghost(state, row, col)
+        elif (self.map[row][col] == MazeEntity.GHOST):
+            return GHOST_REWARD
+        elif (self.map[row][col] == MazeEntity.PACMAN or self.map[row][col] == MazeEntity.EMPTY_CELL):
+            return EMPTY_CELL_REWARD #+ self._manhattan_distance_to_closest_ghost(state, row, col)
 
-    def get_utility(self, row, col):
-        return self.utilities[row][col]
-
-    def is_wall(self, row, col):
-        return self.map[row][col] == WALL_REWARD
+        raise Exception("Oops! Something went wrong")
 
     def print_map(self, print_mode = 0):
         '''
         print a string representation of the map to the terminal
         print_mode:
             0 - human_readable
-            1 - rewards
-            2 - utilities
-
+            1 - utilities
         '''
-        print("---------------MAP---------------")
+        print("---------------------------------------------MAP---------------------------------------------")
         for i in reversed(range(len(self.map))):
             for j in range(len(self.map[i])):
-                if (print_mode == 1):
-                    if self.pac_man_pos[1] == i and self.pac_man_pos[0] == j:
+                if (print_mode == 0):
+                    if self.map[i][j] == MazeEntity.PACMAN:
                         print "P\t",
-                    elif self.map[i][j] == WALL_REWARD:
+                    elif self.map[i][j] == MazeEntity.WALL:
                         print "|\t",
-                    else:
-                        print str(self.map[i][j]) + "\t",
-                elif (print_mode == 0):
-                    if self.pac_man_pos[1] == i and self.pac_man_pos[0] == j:
-                        print "P\t",
-                    elif self.map[i][j] == WALL_REWARD:
-                        print "|\t",
-                    elif self.map[i][j] == FOOD_REWARD:
+                    elif self.map[i][j] == MazeEntity.FOOD:
                         print "*\t",
-                    elif self.map[i][j] == CAPSULE_REWARD:
+                    elif self.map[i][j] == MazeEntity.CAPSULE:
                         print "o\t",
-                    elif self.map[i][j] == GHOST_REWARD:
+                    elif self.map[i][j] == MazeEntity.GHOST:
                         print "@\t",
                     else:
-                        print str(round(self.utilities[i][j], 2)) + "\t",
-
+                        print (str(round(self.utilities[i][j], 2)) if self.utilities else "?")  + "\t",
                 else:
-                    if self.pac_man_pos[1] == i and self.pac_man_pos[0] == j:
-                        print "P\t",
-                    elif self.is_wall(i, j):
+                    if self.map[i][j] == MazeEntity.WALL:
                         print "|\t",
                     else:
-                        print str(self.utilities[i][j]) + "\t",
+                        print (str(round(self.utilities[i][j], 2)) if self.utilities else "?") + "\t",
 
             print ""
 
-    def _value_iteration(self, state, discount_factor = 0.5):
-        self.print_map(1)
-        U = [row[:] for row in self.map]
+    def _value_iteration(self, state):
+        U =  [[0 for i in range(len(self.map[0]))] for j in range(len(self.map))]
+        for row in range(len(U)):
+            for col in range(len(U[row])):
+                #print U[row][col], "TO",
+                U[row][col] = self.get_reward(row, col, state)
+
         U_1 = [row[:] for row in U]
 
-        for i in range(10):
+        for i in range(100):
             U = [row[:] for row in U_1]
 
             for row in range(len(self.map)):
-                for col in range(len(self.map[0])):
-                    #print(row, col, self.map[row][col])
-                    if self.map[row][col] == EMPTY_CELL_REWARD:
-                        U_1[row][col] = self.map[row][col] + 0.5 * max(self.getEU(U, row, col).values())
-
-        print("Finished value iteration \n\n\n")
+                for col in range(len(self.map[row])):
+                    if self.map[row][col] == MazeEntity.EMPTY_CELL or self.map[row][col] == MazeEntity.PACMAN:
+                        #print(self.get_reward(row, col))
+                        U_1[row][col] = self.get_reward(row, col, state) + 1 * max(self.getEU(U, row, col).values())
         self.utilities = U_1
-        self.print_map(0)
+        #self.print_map(1)
+        #self.print_map(0)
 
     def getEU(self, utilities, row, col):
         deterministic_utilities = {
             "Stop": utilities[row][col],
-            "North": utilities[row + 1][col] if self.map[row + 1][col] != WALL_REWARD else utilities[row][col],
-            "East": utilities[row][col + 1] if self.map[row][col + 1] != WALL_REWARD else utilities[row][col],
-            "South": utilities[row - 1][col] if self.map[row - 1][col] != WALL_REWARD else utilities[row][col],
-            "West": utilities[row][col - 1] if self.map[row][col - 1] != WALL_REWARD else utilities[row][col],
+            "North": utilities[row + 1][col] if self.map[row + 1][col] != MazeEntity.WALL else utilities[row][col],
+            "East": utilities[row][col + 1] if self.map[row][col + 1] != MazeEntity.WALL else utilities[row][col],
+            "South": utilities[row - 1][col] if self.map[row - 1][col] != MazeEntity.WALL else utilities[row][col],
+            "West": utilities[row][col - 1] if self.map[row][col - 1] != MazeEntity.WALL else utilities[row][col],
         }
 
         expected_utilities = {
@@ -177,6 +186,15 @@ class MapRepresentation():
 
         return expected_utilities
 
+    def _manhattan_distance_to_closest_ghost(self, state, row, col):
+    	theGhosts = api.ghosts(state)
+
+    	distances = []
+    	for i in range(len(theGhosts)):
+    	    distances.append(util.manhattanDistance([row, col],theGhosts[i]))
+
+    	return min(distances)
+
 
 class MDPAgent(Agent):
     # Constructor: this gets run when we first invoke pacman.py
@@ -188,7 +206,7 @@ class MDPAgent(Agent):
     # game state to access.
     def registerInitialState(self, state):
         # CREATE A MAP THAT STORES THE UTILITY OF EACH CELL
-        self.map = MapRepresentation(state)
+        self.map = Maze(state)
 
     # This is what gets run in between multiple games
     def final(self, state):
@@ -196,19 +214,17 @@ class MDPAgent(Agent):
 
     # For now I just move randomly
     def getAction(self, state):
-        self.map.update_map(state) # performs value iteration
+        self.map.update(state) # performs value iteration
 
         # Get the actions we can try
         legal_moves = api.legalActions(state)
-        print(legal_moves)
         move = self._determine_best_action(state, legal_moves)
         # Random choice between the legal options.
-        print("Moving", move)
+        #print("Chosen Direction:", move)
         return api.makeMove(move, legal_moves)
 
     def _determine_best_action(self, state, legal_moves):
         moves = self.map.getEU(self.map.utilities, api.whereAmI(state)[1], api.whereAmI(state)[0])
-        print("moves", moves)
 
         return self.arg_max({k: v for k, v in moves.iteritems() if k in legal_moves}
 )
